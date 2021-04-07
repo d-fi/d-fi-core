32a3945f6167aa52243ca13523e74386a395e947
Sayem Chowdhury <sayem314@gmail.com>
2021-04-07 17:30:03 +0600

    export playlist channel ttypes

--- src/types/playlist-channel.ts
+++ src/types/playlist-channel.ts
@@ -14,7 +14,7 @@ interface nativeAdsType {
   weight: number; // 1
 }
 
-interface itemsType {
+export interface playlistChannelItemsType {
   item_id: string; // 'page_id=channels/dance,render_id=608a93738223d615200e6390ea48f8f3,version=channel-cms.1.0,section_id=long-card-horizontal-grid,section_position=0,module_id=be55d60e-e3c3-421a-9ea9-aef6f0c63c6c,module_type=playlists,layout=playlists_layout_long-card-horizontal-grid,content_source=playlists_content-source_custom,content_source_count=0,content_programming_count=8,section_content=playlist%3A1291471565%3Bplaylist%3A2249258602%3Bplaylist%3A2113355604%3Bplaylist%3A1950512362%3Bplaylist%3A1495242491%3Bplaylist%3A6090195324%3Bplaylist%3A706093725%3Bplaylist%3A7837492422,item_type=playlist,item_id=1291471565,item_position=0'
   id: string; // '1291471565'
   type: 'playlist';
@@ -52,10 +52,10 @@ interface itemsType {
   };
 }
 
-interface sectionsType {
+export interface playlistChannelSectionsType {
   layout: string; // 'long-card-horizontal-grid'
   section_id: string; // 'page_id=channels/dance,render_id=608a93738223d615200e6390ea48f8f3,version=channel-cms.1.0,section_id=long-card-horizontal-grid,section_position=0,module_id=be55d60e-e3c3-421a-9ea9-aef6f0c63c6c,module_type=playlists,layout=playlists_layout_long-card-horizontal-grid,content_source=playlists_content-source_custom,content_source_count=0,content_programming_count=8,section_content=playlist%3A1291471565%3Bplaylist%3A2249258602%3Bplaylist%3A2113355604%3Bplaylist%3A1950512362%3Bplaylist%3A1495242491%3Bplaylist%3A6090195324%3Bplaylist%3A706093725%3Bplaylist%3A7837492422'
-  items: itemsType[];
+  items: playlistChannelItemsType[];
   title: string; // 'Top Dance & EDM playlists'
   target: string; // '/channels/module/be55d60e-e3c3-421a-9ea9-aef6f0c63c6c'
   related: {
@@ -76,6 +76,6 @@ export interface playlistChannelType {
   };
   title: string; // 'Dance & EDM'
   persistent: boolean;
-  sections: sectionsType[];
+  sections: playlistChannelSectionsType[];
   expire: number; // 1617707131
 }
